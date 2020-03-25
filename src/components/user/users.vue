0564018e99dd230cf91ea2b753e7fde2825427aa
unknown <3373175300@qq.com>
2020-03-25 16:27:04 +0800

    完成权限开发

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -70,7 +70,7 @@
                         <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
                         <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
                         <el-tooltip  effect="dark" content="分配角色" placement="top" :enterable="false">
-                             <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                             <el-button type="warning" icon="el-icon-setting" size="mini" @click="setRole(scope.row)"></el-button>
                             </el-tooltip>
                        
                     </template>
@@ -139,6 +139,32 @@
                 <el-button type="primary" @click="editUserInfo">确 定</el-button>
             </span>
             </el-dialog>
+    <!-- 分配角色对话框 -->
+            <el-dialog
+                title="分配角色"
+                :visible.sync="setRoledialogVisible"
+                width="50%"
+                @close="setRoleDialogClosed"
+                >
+                <div>
+                    <p>当前的用户:{{userInfo.username}}</p>
+                    <p>当前的角色:{{userInfo.role_name}}</p>
+                    <p>分配新角色:
+                         <el-select v-model="selectedRoleId" placeholder="请选择">
+                            <el-option
+                            v-for="item in rolesList"
+                            :key="item.id"
+                            :label="item.roleName"
+                            :value="item.id">
+                            </el-option>
+                        </el-select>
+                    </p>
+                </div>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="setRoledialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
+                </span>
+            </el-dialog>
         </el-card>
     </div>
 </template>
@@ -181,6 +207,10 @@
                 },
                 editDialogVisible:false,
                 editForm:{},
+                setRoledialogVisible:false,
+                userInfo:{},
+                rolesList:[],
+                selectedRoleId:"",
                 addFormRules:{
                     username:[
                         {required: true, message:'请输入用户名', trigger: 'blur'},
@@ -211,7 +241,8 @@
                         {required: true, message:'请输入手机号', trigger: 'blur'},
                         {validator:checkMobile,trigger:"blur"} 
                     ]
-                }
+                },
+
 
             }
         },
@@ -301,6 +332,36 @@
             if(res.meta.status !== 200) return this.$message.error("删除用户失败")
             this.$message.success("删除用户成功")
             this.getUserList()
+            },
+            
+           async setRole(userInfo){
+                this.userInfo=userInfo
+             const {data:res}=await  this.$http.get("roles")
+             if(res.meta.status !==200){
+                 return this.$message.error("获取角色列表失败")
+             }
+             this.rolesList=res.data
+                this.setRoledialogVisible=true
+            },
+           async saveRoleInfo(){
+                 if (!this.selectedRoleId) {
+        return this.$message.error('请选择要分配的角色')
+      }
+                const {data:res}=await this.$http.put(`users/${this.userInfo.id}/role`,{
+                    rid:this.selectedRoleId
+                })
+                console.log(res)
+                if(res.meta.status !== 200){
+                    return this.$message.error("更新角色失败")
+                }
+                this.$message.success("更新角色成功")
+                this.getUserList()
+                this.setRoledialogVisible=false
+
+            },
+            setRoleDialogClosed(){
+                this.selectedRoleId=''
+                this.userInfo={}
             }
         }
     }
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Login from "../components/login/login.vue"
 import Home from "../components/Home.vue"
 import Welcome from "../components/Welcome.vue"
 import Users from "../components/user/users.vue"
-
+import Rights from "../components/power/Rights.vue"
+import Roles from "../components/power/Roles.vue"
 Vue.use(Router)
 
 const router= new Router({
@@ -23,6 +24,12 @@ const router= new Router({
         },{
           path:"/users",
           component:Users
+        },{
+          path:"/rights",
+          component:Rights
+        },{
+          path:"/roles",
+          component:Roles
         }
       ]
     },
